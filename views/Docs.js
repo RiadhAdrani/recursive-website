9087390073cee1ec3ada9bd30ddfedf1877ff131
adrani_laptop <riadh_adrani@hotmail.fr>
2021-12-09 21:21:19 +0100

    fixed a bug preventing github-pages from importing certain files named with an underscore

--- views/Docs.js
+++ views/Docs.js
@@ -1,5 +1,33 @@
-import DocTemplate from "../components/templating/DocTemplate.js";
+import DocSection from "../components/section/DocSection.js";
+import WelcomeTop from "../components/section/WelcomeTop.js";
+import DividerView from "../recursivejs/createcomponent/components/text/DividerView.js";
+import { A } from "../recursivejs/createcomponent/Components.js";
 
 export default () => {
-     return DocTemplate();
+     return DividerView({
+          styleSheet: {
+               className: "docs-view",
+               normal: {
+                    display: "flex",
+                    flexDirection: "column",
+               },
+          },
+          children: [
+               WelcomeTop({
+                    title: "Documentation",
+                    subtitle: "A deep dive",
+               }),
+               DocSection({
+                    title: "Ouch ! Section is under construction",
+                    text: [
+                         "To get started, you can follow the README of the github repository ",
+                         A({
+                              children: "Here",
+                              href: "https://github.com/RiadhAdrani/recursive#getting-started",
+                              target: "blank",
+                         }),
+                    ],
+               }),
+          ],
+     });
 };
